f18b476227e37a44b303cb85f1bc5011b5c8f07d
Bachkoi <sbb7355@rit.edu>
2023-04-04 10:39:35 -0400

    Added in more changes to the economics of the game. Need to merge in the new UI art to replace the placeholder we have in the game rn.

--- IGME603-4-4/Assets/Scripts/Player.cs
+++ IGME603-4-4/Assets/Scripts/Player.cs
@@ -38,10 +38,13 @@ public class Player : MonoBehaviour
     [SerializeField] Button[] shopButtons;
     [SerializeField] Text[] shopText;
     [SerializeField] GameObject shopUI;
+    [SerializeField] GameObject obstaclesHolder;
+    [SerializeField] GameObject revolver;
     public float fireRatePrice = 100.0f;
     public float accuracyPrice = 500.0f;
     public float reloadSpeedPrice = 1000.0f;
     public float ammoCountPrice = 1000.0f;
+    bool countedCash = false;
 
     Ray raycast;
     public Text scoreText;
@@ -51,7 +54,19 @@ public class Player : MonoBehaviour
     public Text gameOverText;
     public Text targetsHitText;
     public Text targetsSeenText;
+
     public Text cashText;
+
+    public Text playerFireRate;
+    public Text playerAmmoCount;
+    public Text playerReloadSpeed;
+    public Text playerAccuracy;
+
+    public Text upgradeFireRate;
+    public Text upgradeAmmoCount;
+    public Text upgradeReloadSpeed;
+    public Text upgradeAccuracy;
+
 
 
 
@@ -75,12 +90,17 @@ public class Player : MonoBehaviour
         if(timeRemaining <= 0)
         {
             timeText.text = "Time Remaining: 0.0";
+            if(countedCash == false)
+            {
+                CalculateCash();
+                countedCash = true;
+            }
 
         }
         if (timeRemaining > 0.0f)
         {
 
-            timeText.text = "Time Remaining: " + timeRemaining;
+            timeText.text = "Time Remaining: " + timeRemaining.ToString("n2");
             ammoCount.text = "Ammo: " + ammo;
             if(shotsTaken > 0)
             {
@@ -104,8 +124,7 @@ public class Player : MonoBehaviour
 
             }
             crosshair.transform.position = worldMousePos;
-            //reddickle.transform.position = Camera.main.WorldToScreenPoint(Input.mousePosition);
-            //crosshair.transform.position = Camera.main.WorldToScreenPoint(Input.mousePosition);
+
             shotClock += Time.deltaTime;
             scoreText.text = "Score: " + score;
             switch (maxAmmo)
@@ -194,7 +213,6 @@ public class Player : MonoBehaviour
             shopButton.gameObject.SetActive(true);
             restartButton.gameObject.SetActive(true);
             restartButton.onClick.AddListener(RestartGame);
-            shopButton.onClick.AddListener(CalculateCash);
             shopButton.onClick.AddListener(OpenShop);
         }
 
@@ -202,7 +220,7 @@ public class Player : MonoBehaviour
 
     void RestartGame()
     {
-        timeRemaining = 10.0f;
+        timeRemaining = 60.0f;
         paused = false;
         restartButton.gameObject.SetActive(false);
         shopButton.gameObject.SetActive(false);
@@ -214,7 +232,9 @@ public class Player : MonoBehaviour
         shotsTaken = 0.0f;
         score = 0.0f;
         ammo = maxAmmo;
+        countedCash = false;
         manager.GetComponent<EnemyManager>().targetsSpawned = 0;
+
         foreach(GameObject sixShot in sixShotReload)
         {
             sixShot.SetActive(false);
@@ -232,11 +252,8 @@ public class Player : MonoBehaviour
 
     void Shoot()
     {
-        //Debug.Log("TEST");
         Vector3 tempMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Debug.Log("PRERNG " + tempMousePos);
-        //raycast = Camera.main.ScreenPointToRay(tempMousePos);
-        //Debug.Log(raycast);
+
 
         if (hardMode)
         {
@@ -246,38 +263,19 @@ public class Player : MonoBehaviour
             tempMousePos.y *= rng;
             rng = Random.Range(0.7f, shotAccuracy);
             tempMousePos.z *= rng;
-            //Debug.Log(Input.mousePosition);
-            //Debug.Log("randomed" + tempMousePos);
         }
-        //float rng = Random.Range(0.0f,shotAccuracy);
-        //tempMousePos.x *= rng;
-        //rng = Random.Range(0.0f, shotAccuracy);
-        //tempMousePos.y *= rng;
-        //rng = Random.Range(0.0f, shotAccuracy);
-        //tempMousePos.z *= rng;
-        //Debug.Log(Input.mousePosition);
-        //Debug.Log("randomed" + tempMousePos);
         shotsTaken++;
 
-        //raycast = Camera.main.ScreenPointToRay(tempMousePos);
         int bulletRng = Random.Range(0, bulletHoles.Length);
         Vector3 tempVec3 = new Vector3(tempMousePos.x, tempMousePos.y, 0.0f);
         GameObject testBulletHole = Instantiate(bulletHoles[bulletRng], tempVec3, transform.rotation);
         testBulletHole.SetActive(true);
         testBulletHole.GetComponent<BulletHole>().lifeSpan = 3.0f;
-        //spawnedGameObjects.Add(testBulletHole);
-        //bulletHoles[bulletRng].SetActive(true);
-        //bulletHoles[bulletRng].transform.position = new Vector3(tempMousePos.x, tempMousePos.y, 0);
-        //bulletHoles[bulletRng].GetComponent<BulletHole>().lifeSpan = 3.0f;
         foreach (GameObject enemy in manager.GetComponent<EnemyManager>().staticEnemies)
         {
             if (enemy.activeSelf)
             {
-                //Debug.Log(enemy.name);
-                //Debug.Log("max" + enemy.GetComponent<BoxCollider2D>().bounds.max);
-                //Debug.Log("min" + enemy.GetComponent<BoxCollider2D>().bounds.min);
-                //Debug.Log("TempX " + tempMousePos.x);
-                //Debug.Log("TempY " + tempMousePos.y);
+
                 if ((enemy.GetComponent<BoxCollider2D>().bounds.max.x > tempMousePos.x) && (enemy.GetComponent<BoxCollider2D>().bounds.min.x < tempMousePos.x))
                 {
                     if ((enemy.GetComponent<BoxCollider2D>().bounds.max.y > tempMousePos.y) && (enemy.GetComponent<BoxCollider2D>().bounds.min.y < tempMousePos.y)){
@@ -290,41 +288,11 @@ public class Player : MonoBehaviour
                 }
             }
         }
-
-        //Debug.Log(raycast);
-        //Debug.Log(Physics.Raycast(raycast, out RaycastHit hit));
-        //RaycastHit2D hit2D = Physics2D.Raycast(tempMousePos,Vector2.;
-        //if (Physics.Raycast(raycast, out RaycastHit hit2))
-        //{
-        //    GameObject hitEnemy = hit2.collider.gameObject;
-        //    Debug.Log(hitEnemy);
-        //    if(hitEnemy != null )
-        //    {
-        //        if (hitEnemy.tag.Equals("Enemy"))
-        //        {
-        //            if (hitEnemy.GetComponent<StaticEnemy>() != null)
-        //            {
-        //                hitEnemy.SetActive(false);
-        //                score += 100.0f;
-        //                manager.GetComponent<EnemyManager>().activeStatic--;
-        //            }
-        //        }
-        //    }
-        //}
         // Play sound
         soundEffects.clip = sounds[1];
         soundEffects.Play();
     }
-    //void RemoveInactives()
-    //{
-    //    foreach(GameObject tempGO in spawnedGameObjects)
-    //    {
-    //        if(tempGO.activeSelf == false)
-    //        {
-    //            Destroy(tempGO);
-    //        }
-    //    }
-    //}
+
     void rapidFireShot()
     {
 
@@ -341,14 +309,13 @@ public class Player : MonoBehaviour
     void CalculateCash()
     {
         float tempCashGained = 0.0f;
-        tempCashGained += targetsHitCount;
+        tempCashGained += (shotsHit * 20.0f);
         tempCashGained += (accuracy * 2.0f) / 100.0f;
-        if((targetsHitCount/targetsSeenCount) > 0.70)
+        if((shotsHit / targetsSeenCount) > 0.70)
         {
             tempCashGained *= 1.5f;
         }
         cash += tempCashGained;
-        shopButton.gameObject.SetActive(false);
     }
 
     void OpenShop()
@@ -372,10 +339,9 @@ public class Player : MonoBehaviour
         {
             twelveShot.SetActive(false);
         }
-        foreach(GameObject obstacle in manager.GetComponent<EnemyManager>().obstacles)
-        {
-            obstacle.SetActive(false);
-        }
+        obstaclesHolder.SetActive(false);
+        crosshair.SetActive(false);
+        revolver.SetActive(false);
     }
 
     public void BuyFireRate()
@@ -388,6 +354,8 @@ public class Player : MonoBehaviour
                 shotDelay -= 0.1f;
                 fireRatePrice *= 2.0f;
                 cashText.text = "$" + cash;
+                upgradeFireRate.text = "Cost: $ " + fireRatePrice;
+                playerFireRate.text = "Your fire rate is " + shotDelay + " seconds";
             }
             else
             {
@@ -406,6 +374,8 @@ public class Player : MonoBehaviour
                 shotAccuracy += 0.1f;
                 accuracyPrice *= 2.0f;
                 cashText.text = "$" + cash;
+                upgradeAccuracy.text = "Cost: $ " + accuracyPrice; 
+                playerAccuracy.text = "Your gun is now " + accuracy + "% accurate";
             }
             else
             {
@@ -424,13 +394,17 @@ public class Player : MonoBehaviour
                 maxAmmo = 8;
                 ammoCountPrice *= 2.0f;
                 cashText.text = "$" + cash;
+                upgradeAmmoCount.text = "Cost: $ " + ammoCountPrice;
+                playerAmmoCount.text = "Your revolver now has " + maxAmmo + " shots in it.";
             }
-            if (maxAmmo == 8)
+            else if (maxAmmo == 8)
             {
                 cash -= ammoCountPrice;
                 maxAmmo = 12;
                 ammoCountPrice *= 2.0f;
                 cashText.text = "$" + cash;
+                upgradeAmmoCount.text = "Cost: $ " + ammoCountPrice;
+                playerAmmoCount.text = "Your revolver now has " + maxAmmo + " shots in it.";
             }
             else
             {
@@ -447,6 +421,7 @@ public class Player : MonoBehaviour
             {
                 // Speed up the reload
                 Debug.Log("Reload sped up");
+                upgradeReloadSpeed.text = "Cost: $ " + reloadSpeedPrice;
             }
             else
             {
@@ -458,6 +433,8 @@ public class Player : MonoBehaviour
     public void CloseShop()
     {
         shopUI.SetActive(false);
-
+        obstaclesHolder.SetActive(true);
+        crosshair.SetActive(true);
+        revolver.SetActive(true);
     }
 }
